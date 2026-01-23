f70c887ca60640af788d476a380ebbd8d3009759
W4lloF <W4llof@gmail.com>
2026-01-23 12:33:56 +0100

    Ajout Header et Footer

---
+++ src/Controller/CoachingController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CoachingController extends AbstractController
+{
+    #[Route('/coaching', name: 'app_coaching')]
+    public function index(): Response
+    {
+        return $this->render('coaching/index.html.twig', [
+            'controller_name' => 'CoachingController',
+        ]);
+    }
+}
